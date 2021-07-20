d74399181bcf0143f5e0fafe2fbbe0bbd84575b3
kbittner15 <kb51710n@pace.edu>
2021-07-20 00:22:05 -0400

    database working

---
+++ src/App/core/config.js
@@ -0,0 +1,17 @@
+export const firebase = {  
+  apiKey: "AIzaSyAS_v-QVi_3v84VFOnkF4nJ0-g0Ye4OwSk",
+  authDomain: "cliqd-19df5.firebaseapp.com",
+  databaseURL: "https://cliqd-19df5-default-rtdb.firebaseio.com",
+  projectId: "cliqd-19df5",
+  storageBucket: "cliqd-19df5.appspot.com",
+  messagingSenderId: "440529118050",
+  appId: "1:440529118050:web:1788a1ed3477f50664423b",
+  measurementId: "G-DB5HZKE6DP"}
+
+  export const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true // Store in Firestore instead of Real Time DB
+  }
+  
+  export default { firebase, rrfConfig }
+  
